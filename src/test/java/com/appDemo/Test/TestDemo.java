d64d3661331330ca357530266c56a0b8ab70b6e9
Abhinav Srivastava <dell@DESKTOP-HH25O9G>
2024-08-30 13:16:08 +0530

    My First Commit on git

---
+++ src/test/java/com/appDemo/Test/TestDemo.java
@@ -0,0 +1,25 @@
+package com.appDemo.Test;
+
+import org.testng.annotations.Test;
+
+public class TestDemo {
+
+	@Test
+	public void TestCase1() {
+
+		System.out.println("This is Test Case - 1");
+	}
+
+	@Test
+	public void TestCase2() {
+
+		System.out.println("This is Test Case - 2");
+	}
+
+	@Test
+	public void TestCase3() {
+
+		System.out.println("This is Test Case - 3");
+	}
+
+}
---
+++ src/test/java/com/appDemo/Test/TestDemo1.java
@@ -0,0 +1,25 @@
+package com.appDemo.Test;
+
+import org.testng.annotations.Test;
+
+public class TestDemo1 {
+
+	@Test
+	public void TestCase4() {
+
+		System.out.println("This is Test Case - 4");
+	}
+
+	@Test
+	public void TestCase5() {
+
+		System.out.println("This is Test Case - 5");
+	}
+
+	@Test
+	public void TestCase6() {
+
+		System.out.println("This is Test Case - 6");
+	}
+
+}
